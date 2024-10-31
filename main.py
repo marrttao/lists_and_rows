e534704c0c12245c39374e87943e442b56f820cd
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 22:42:14 +0200

    completed task 1

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+def reverse(row):
+    reversed_row = []
+    for i in range(len(row) - 1, -1, -1):
+        reversed_row.append(row[i])
+    return reversed_row
+
+
 if __name__ == '__main__':
-    pass
+    row = []
+    while True:
+        element = input("Fill up your row (enter to finish): ")
+        if element.isdigit():
+            row.append(int(element))
+        elif element == "":
+            break
+        else:
+            print("Please enter a valid number.")
+    reversed_row = reverse(row)
+    print("Original row:", row)
+    print("Reversed row:", reversed_row)
