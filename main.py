867a4417b62ecd380a713308b11fb973ef677642
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 23:35:11 +0200

    completed task 3

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+
+def number_of_word(row, word):
+    count = 0
+    word = word.lower()
+    for i in range(len(row)):
+        if word in row[i].lower():
+            count += 1
+    print(f"{word} {count} times in the row")
+
 if __name__ == '__main__':
-    row = []
+    _row = []
     while True:
         element = input("Fill up your row (enter to finish): ")
         if element == "":
             break
-        row.append(element)
+        _row.append(element)
+    _word = input("word you want to count: ")
+    number_of_word(_row, _word)
