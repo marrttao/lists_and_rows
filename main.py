f7973ee4420f072f8a459e5ce6c05fde5464ab08
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-01 00:08:22 +0200

    completed task 5

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+def number_of_word(row, word, replace):
+    count = 0
+    word = word.lower()
+    for i in range(len(row)):
+        element = row[i].lower()
+        index = 0
+        while index < len(element):
+            index = element.find(word, index)
+            if index == -1:
+                break
+            row[i] = row[i][:index] + replace + row[i][index + len(word):]
+            index += len(word)
+    print(row)
+
 if __name__ == '__main__':
-    row = []
+    _row = []
     while True:
         element = input("Fill up your row (enter to finish): ")
         if element == "":
             break
-        row.append(element)
+        _row.append(element)
+    _word = input("word you want to replace: ")
+    _replace = input("replacing word: ")
+    number_of_word(_row, _word, _replace)
