8a60860f4866a35770aee1845d5aa1629f5b42a4
Maris Shardyko <marrttaoburberry@gmail.com>
2024-10-31 23:16:42 +0200

    start of code

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 if __name__ == '__main__':
-    pass
+    row = []
+    while True:
+        element = input("Fill up your row (enter to finish): ")
+        if element == "":
+            break
+        row.append(element)
