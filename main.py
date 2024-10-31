74fb51765bee4e61e182811fcf292b74126aa5ab
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-01 00:01:49 +0200

    solved little problems

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def number_of_word(row, word):
     count = 0
     word = word.lower()
     for i in range(len(row)):
-        for char in row[i].lower():
-            if char == word:
-                count += 1
+        element = row[i].lower()
+        index = 0
+        while index < len(element):
+            index = element.find(word, index) #Знаю как работате если нужно будет смогу обьяснмить
+            if index == -1:
+                break
+            count += 1
+            index += len(word)
     print(f"{word} {count} times in the row")
 
 if __name__ == '__main__':
